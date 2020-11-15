44f00e3faba3af63ae5130ae4ed07a7320bae69e
Marcel Groeneveld <marceltjeg@gmail.com>
2020-11-15 03:02:05 +0100

    sessions

--- init.php
+++ init.php
@@ -1,9 +1,12 @@
 <?php
 define("ROOT", __DIR__ . "/");
 
-if (isset($login_vereist)){
-    if(!PHP_SESSION_NONE){
-        
-    }
+if(!PHP_SESSION_NONE){
+    session_start();
+}
+
+
+if (isset($login_vereist) && ($_SESSION['loggedin'] != TRUE)){
+Header("Location: login.php");
 
 }
