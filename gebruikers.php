07ee48ae41d5a3b4302ce9917d84687fd303b30e
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 00:12:28 +0100

    api

--- gebruikers.php
+++ gebruikers.php
@@ -108,7 +108,6 @@
   <script src="js/front.js"></script>
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
   <script>
-      * Formatting function for row details - modify as you need */
       function format ( d ) {
           // `d` is the original data object for the row
           return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
