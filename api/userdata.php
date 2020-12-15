3478bdfc8f8296d49610c9c5982586c22e0f1e80
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 00:19:53 +0100

    api

--- api/userdata.php
+++ api/userdata.php
@@ -1,1 +1,25 @@
 <?php
+$JsonOutArray[] = array();
+if ($stmt = $con->prepare('SELECT gebruiker_id, Voornaam, achternaam, Email FROM Gebruikers')) {
+    // Bind parameters (s = string, i = int, b = blob)
+    $stmt->execute();
+
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $voornaam, $achternaam, $email, $wachtwoord);
+        $stmt->fetch();
+
+        while ($row_data = $stmt->fetch_assoc()) {
+
+            $JsonOutArray[] = array(
+                "uid" => $row_data['gebruiker_id'],
+                "voornaam" => $row_data['Voornaam']
+
+            );
+        }
+        return $JsonOutArray;
+    }
+}
+$stmt->close();
+
