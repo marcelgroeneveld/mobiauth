fd67001ea2a0b7ac0ea28b0223f3ee9f56f4c55e
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 00:22:25 +0100

    api

--- api/userdata.php
+++ api/userdata.php
@@ -1,4 +1,13 @@
 <?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'mobiauth';
+$DATABASE_PASS = '2O9n9f*n';
+$DATABASE_NAME = 'mobiauth';
+
+
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
 $JsonOutArray[] = array();
 if ($stmt = $con->prepare('SELECT gebruiker_id, Voornaam, achternaam, Email FROM Gebruikers')) {
     // Bind parameters (s = string, i = int, b = blob)
