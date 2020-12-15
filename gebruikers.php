acf04302464ecdd6aa4ea3885b52f39ab574aede
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-15 21:32:39 +0100

    dfghd

--- gebruikers.php
+++ gebruikers.php
@@ -441,5 +441,25 @@
   <script src="vendor/chart.js/Chart.min.js"></script>
   <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
   <script src="js/front.js"></script>
+
+  <script>
+      $(document).ready(function (){
+          var table = $('#example').DataTable({
+              'responsive': true
+          });
+
+          // Handle click on "Expand All" button
+          $('#btn-show-all-children').on('click', function(){
+              // Expand row details
+              table.rows(':not(.parent)').nodes().to$().find('td:first-child').trigger('click');
+          });
+
+          // Handle click on "Collapse All" button
+          $('#btn-hide-all-children').on('click', function(){
+              // Collapse row details
+              table.rows('.parent').nodes().to$().find('td:first-child').trigger('click');
+          });
+      });
+  </script>
   </body>
 </html>
