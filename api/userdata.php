c927df30928bd2e7a7b5046123f16085a0f7d5ec
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 00:34:10 +0100

    api

--- api/userdata.php
+++ api/userdata.php
@@ -17,9 +17,10 @@ if ($stmt = $con->prepare('SELECT gebruiker_id, Voornaam, achternaam, Email FROM
 
     if ($stmt->num_rows > 0) {
         $stmt->bind_result($id, $voornaam, $achternaam, $email);
-        echo 'ja';
+
     }
     while ($stmt->fetch()) {
+        echo 'ja';
         $JsonOutArray[] = array(
             "uid" => $id,
             "voornaam" => $voornaam,
