85f39076f6b3a01a714d52873224ed6cc1f7bbba
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 01:20:36 +0100

    api

--- gebruikers.php
+++ gebruikers.php
@@ -110,20 +110,7 @@
   <script>
       function format ( d ) {
           // `d` is the original data object for the row
-          return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-              '<tr>'+
-              '<td>Full name:</td>'+
-              '<td>'+d.uid+'</td>'+
-              '</tr>'+
-              '<tr>'+
-              '<td>Extension number:</td>'+
-              '<td>'+d.uid+'</td>'+
-              '</tr>'+
-              '<tr>'+
-              '<td>Extra info:</td>'+
-              '<td>And any further details here (images etc)...</td>'+
-              '</tr>'+
-              '</table>';
+          return '';
       }
 
       $(document).ready(function() {
