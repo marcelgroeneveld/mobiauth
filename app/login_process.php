3be089209c78050e59353ff033ed5af40002d872
Marcel Groeneveld <marceltjeg@gmail.com>
2020-12-16 15:38:00 +0100

    dfghsdfgsd

--- app/login_process.php
+++ app/login_process.php
@@ -5,47 +5,34 @@ if(isset($_POST)) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $result = $conn->query("SELECT * FROM Gebruikers WHERE Email = '$email' AND Wachtwoord = '$password'");
+    if ($stmt = $con->prepare('SELECT Email, Wachtwoord FROM Gebruikers WHERE Email = ?')) {
+        // Bind parameters (s = string, i = int, b = blob)
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
 
-    if (mysqli_num_rows($result) == 1) {
-        session_start();
-        $_SESSION['email'] = $email;
-        $_SESSION['password'] = $password;
-        header("Location:card.php");
-    } else {
-        header("Location:index.php");
-        exit;
-    }
-} else {
-    header("Location:index.php");
-    exit;
-}
+        $stmt->store_result();
 
-if ($stmt = $con->prepare('SELECT Email, Wachtwoord FROM Gebruikers WHERE Email = ?')) {
-    // Bind parameters (s = string, i = int, b = blob)
-    $stmt->bind_param('s', $email);
-    $stmt->execute();
-
-    $stmt->store_result();
-
-    if ($stmt->num_rows > 0) {
-        $stmt->bind_result($email, $wachtwoord);
-        $stmt->fetch();
-        if (password_verify($_POST['password'], $wachtwoord)) {
-            session_start();
-            $_SESSION['email'] = $email;
-            $_SESSION['password'] = $password;
-            header("Location:card.php");
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($email, $wachtwoord);
+            $stmt->fetch();
+            if (password_verify($_POST['password'], $wachtwoord)) {
+                session_start();
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+                header("Location:card.php");
+            } else {
+                // Hier is wachtwoord onjuist
+                header("Location:index.php");
+            }
         } else {
-            // Hier is wachtwoord onjuist
+            // hier is gebruikersnaam niet juist
             header("Location:index.php");
         }
-    } else {
-        // hier is gebruikersnaam niet juist
-        header("Location:index.php");
+
+        $stmt->close();
+    }else {
     }
 
-    $stmt->close();
-}else {
 }
+
 
