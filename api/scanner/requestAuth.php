0fa9f151466e9494d8d093afe327162b172c0e86
Marcel Groeneveld <marcel@mgroeneveld.nl>
2021-01-16 22:58:21 +0100

    gsdfgsdf

--- api/scanner/requestAuth.php
+++ api/scanner/requestAuth.php
@@ -1,5 +1,5 @@
 <?php
-
+include("auth.php");
 $deurnaam = $_POST['device_name'];
 $org_id   = $_POST['org_id'];
 
@@ -10,5 +10,14 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if ( mysqli_connect_errno() ) {
     // If there is an error with the connection, stop the script and display the error.
     echo 'error';
-
 }
+if ($stmt = $con->prepare('SELECT gebruiker_id, Voornaam, achternaam, Email, Organisatie FROM Gebruikers')) {
+    // Bind parameters (s = string, i = int, b = blob)
+    $stmt->execute();
+
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $voornaam, $achternaam, $email, $organisatie);
+
+    }
